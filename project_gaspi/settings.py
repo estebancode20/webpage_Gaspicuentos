60ad4c8c4ff0ae7c73686b66821aaa0f3dfcf256
esteban <relbigote75@gmail.com>
2023-10-26 08:32:00 -0300

    inicio prueba actualizacion de producto

--- project_gaspi/settings.py
+++ project_gaspi/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project_gaspi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'example_libro_1',
+        'USER': 'random',
+        'PASSWORD': '235',
+        'HOST': 'localhost',  # O la dirección IP del servidor de la base de datos
+        'PORT': '3306',
     }
 }
 
--- ventas/models.py
+++ ventas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+    ISBN = models.CharField(max_length=20, primary_key=True)
+    titulo = models.CharField(max_length=255)
+    autor = models.CharField(max_length=255, null=True, blank=True)
+    editorial = models.CharField(max_length=255, null=True, blank=True)
+    categoria_codigo = models.CharField(max_length=36, null=True, blank=True)
+    imagen = models.CharField(max_length=255, null=True, blank=True)
+    cantidad_disponible = models.IntegerField(null=True, blank=True)
