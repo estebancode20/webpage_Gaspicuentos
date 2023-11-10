40515037fef5996e508e5290c825022c1da0b455
esteban <relbigote75@gmail.com>
2023-11-10 16:01:39 -0300

    ingreso correcto de datos

--- ventas/models.py
+++ ventas/models.py
@@ -17,7 +17,7 @@ class Libro(models.Model):
     autor = models.CharField(max_length=255, null=True, blank=True)
     editorial = models.CharField(max_length=255, null=True, blank=True)
     categoria_codigo = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True)
-    imagen = models.CharField(max_length=255, null=True, blank=True)
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name ="Imagen",null=True)
     cantidad_disponible = models.IntegerField(null=True, blank=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
