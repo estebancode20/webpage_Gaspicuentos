400c3d6ab8e8582c9f99fe09c85edc8c3c70372c
esteban <relbigote75@gmail.com>
2023-11-10 21:26:30 -0300

    mantenedores admin libros y categorias funcionando

--- ventas/models.py
+++ ventas/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Categoria(models.Model):
     categoria_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    nombre = models.CharField(max_length=255)
-    descripcion = models.TextField()
+    nombre = models.CharField(max_length=255) 
 
     def __str__(self):
         return self.nombre
@@ -16,7 +15,7 @@ class Libro(models.Model):
     titulo = models.CharField(max_length=255)
     autor = models.CharField(max_length=255, null=True, blank=True)
     editorial = models.CharField(max_length=255, null=True, blank=True)
-    categoria_codigo = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True)
     imagen = models.ImageField(upload_to='imagenes/', verbose_name ="Imagen",null=True)
     cantidad_disponible = models.IntegerField(null=True, blank=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
