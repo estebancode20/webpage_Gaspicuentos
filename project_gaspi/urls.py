5c9388781d621f4f109d510abcaa87a6b7d6d11f
esteban <relbigote75@gmail.com>
2023-11-13 03:47:21 -0300

    carga correcta de imagenes de producto

--- project_gaspi/urls.py
+++ project_gaspi/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from ventas import views
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('', views.home, name= 'home'),
     path('admin/', admin.site.urls),
     path("ventas/", include("ventas.urls"))
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
